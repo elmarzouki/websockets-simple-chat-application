42391893a66aa9056dd241d6ae558b6f987c0b85
Mostafa El-Marzouki <mostafam.elmarzouki@gmail.com>
2017-10-19 15:21:51 +0200

    Receiving Messages on Server

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # WebSockets-tutorial
 simple chat application using websockets 
+```
+cd server
+node index.js
+```
+then open index.html in your browser.
--- server/index.js
+++ server/index.js
@@ -1,2 +1,12 @@
 var server = require('ws').Server;
-var sr = new server({ port: 5001 })
+var serve = new server({ port: 5001 })
+
+serve.on('connection', function(wsos){
+    wsos.on('message', function(message) {
+        console.log('Received:', message);
+
+        if(message === 'Hello') {
+            wsos.send('Hey there from the server!');
+        }
+    });
+});
